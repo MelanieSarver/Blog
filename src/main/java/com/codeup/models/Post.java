52bca3968ff5440fd9ca6b69f5906b184b717f06
Melanie Sarver <melanie.sarver@gmail.com>
2017-02-08 14:03:10 -0600

    Exercise 8.1.2
    Created a Post model with getters and setters for id, title, and body

---
+++ src/main/java/com/codeup/models/Post.java
@@ -0,0 +1,34 @@
+package com.codeup.models;
+
+/**
+ * @author Created by mel on 2/8/17.
+ */
+public class Post {
+    private int id;
+    private String title;
+    private String body;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
