0ae149e4b454c55e5ac4b08446ac85dbb2ea7467
Melanie Sarver <melanie.sarver@gmail.com>
2017-02-10 14:39:19 -0600

    Controller
    Implemented the repository through @Autowire and postsDao.
    Changed the mapping from "/posts/" to "/post" where a single post is shown
    Removed @responsebody and added actual functionality to the views.
    Created @get and @post mapping for the edit view.
    Created @postmapping for the delete feature.

--- src/main/java/com/codeup/controllers/PostController.java
+++ src/main/java/com/codeup/controllers/PostController.java
@@ -1,37 +1,67 @@
 package com.codeup.controllers;
 
+import com.codeup.models.Post;
+import com.codeup.repositories.Posts;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Created by mel on 2/7/17.
  */
 @Controller
 public class PostController {
+    @Autowired
+    Posts postsDao;
+
+    public PostController(Posts postsDao) {
+        this.postsDao = postsDao;
+    }
+
     @GetMapping("/posts")
-    @ResponseBody
-    public String index() {
-        return "<h1>Posts index page</h1>";
+    public String index(Model model) {
+        model.addAttribute("posts", postsDao.findAll());
+        return "/posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String post(@PathVariable int id) {
-       return  "<h1>View an individual post<h1>";
+    public String post(@PathVariable int id, Model model) {
+        model.addAttribute("post", postsDao.findOne(id));
+        return  "/posts/show";
     }
 
     @GetMapping("/posts/create")
-    @ResponseBody
-    public String create() {
-        return "<h1>View the form for creating a post<h1>";
+    public String view(Model model) {
+        Post post = new Post();
+        model.addAttribute("post", post);
+        return "posts/create";
     }
 
     @PostMapping("/posts/create")
-    @ResponseBody
-    public String view() {
-        return "<h1>Create a new Post";
+    public String create(@ModelAttribute Post post, Model model) {
+        postsDao.save(post);
+        model.addAttribute("post", post);
+        return "redirect:/posts";
     }
+
+    @GetMapping("/posts/{id}/edit")
+    public String edit(@PathVariable int id, @ModelAttribute Post post, Model model) {
+        model.addAttribute("post", postsDao.findOne(id));
+        return "posts/edit";
+    }
+
+    @PostMapping("/posts/{id}/edit")
+    public String update(@ModelAttribute Post post, Model model) {
+        postsDao.save(post);
+        model.addAttribute("post", post);
+        return "redirect:/posts";
+    }
+
+    @PostMapping("/posts/delete")
+    public String deletePost(@ModelAttribute Post post) {
+        postsDao.delete(postsDao.findOne(post.getId()));
+        return "redirect:/posts";
+    }
+
 }
