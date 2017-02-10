ec88634227362aab0969d6dd1b657fcdf1e5f126
Melanie Sarver <melanie.sarver@gmail.com>
2017-02-10 14:40:59 -0600

    Entity
    Implemented hibernate functionality to create the database table posts.

--- src/main/java/com/codeup/models/Post.java
+++ src/main/java/com/codeup/models/Post.java
@@ -1,24 +1,32 @@
 package com.codeup.models;
 
+import javax.persistence.*;
+
 /**
  * @author Created by mel on 2/8/17.
  */
+@Entity
+@Table(name = "posts")
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(nullable = false)
     private String title;
+    @Column(nullable = false, length = 2000)
     private String body;
 
-    public long getId() {
-        return id;
+    public Post(String title, String body) {
+        this.title = title;
+        this.body = body;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public Post() {
+
     }
 
-    public String getTitle() {
-        return title;
-    }
+    public String getTitle() { return title; }
 
     public void setTitle(String title) {
         this.title = title;
@@ -31,4 +39,8 @@ public class Post {
     public void setBody(String body) {
         this.body = body;
     }
+
+    public int getId() { return id;}
+
+    public void setId(int id) { this.id = id; }
 }
